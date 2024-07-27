762608d78c173488ca687b144e4159c8cc71419e
Georgio Mazraani <mazraanigeorgio@gmail.com>
2024-07-27 17:38:14 +0300

    C2

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ fetch('https://jsonplaceholder.typicode.com/users')
 
         data.forEach(element => {
             let user = document.createElement('option');
+            user.value = JSON.stringify({
+                username: element.username,
+                email: element.email
+            });
             user.innerHTML = element.name;
             users.appendChild(user);
         });
@@ -12,10 +16,20 @@ fetch('https://jsonplaceholder.typicode.com/users')
 
 document.getElementById('btn').addEventListener('click', () => {
     let users = document.getElementById('users');
+    let table = document.querySelector('#usersTable tbody');
+    let row = document.createElement('tr');
+    let nameCell = document.createElement('td');
+    let unCell = document.createElement('td');
+    let emailCell = document.createElement('td');
     let selectedUserName = users.options[users.selectedIndex].text;
-    let p = document.getElementById('para');
-    let name = document.createElement('p');
-    p.innerHTML = "";
-    name.innerHTML = `User chosen is ${selectedUserName}`;
-    p.appendChild(name);
+    let selectedUserData = JSON.parse(users.options[users.selectedIndex].value);
+
+    nameCell.innerHTML = selectedUserName;
+    unCell.innerHTML = selectedUserData.username;
+    emailCell.innerHTML = selectedUserData.email;
+    row.appendChild(nameCell);
+    row.appendChild(unCell);
+    row.appendChild(emailCell);
+    table.appendChild(row);
 });
+
